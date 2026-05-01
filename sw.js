e6880a6fec8ffb0df8c4f784911462bfcab6a1b1
SalmanDesaign-er <sekretariat.salmantauhid@gmail.com>
2026-05-01 16:30:30 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 // UBAH ANGKA INI SETIAP KALI BOS UPDATE index.html (Misal: v2, v3, v4)
-const CACHE_NAME = 'absen-harian-Master-v1'; 
+const CACHE_NAME = 'absen-harian-Master-v1.3'; 
 
 const urlsToCache = [
+ './',
   './index.html',
   './manifest.json',
   'https://cdn.tailwindcss.com',
